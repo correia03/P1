5933347232459ed33cde8258f0060f0bfe9c0d63
correia03 <correialuis@ipvc.pt>
2023-05-13 18:05:39 +0100

    adicionar produtos e categorias

--- Listas.h
+++ Listas.h
@@ -1,2 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+typedef struct categoria
+{
+    char nome[100];
+    char identificador[1];
+} categoria;
+//MAXIMO 250 PRODUTOS
+typedef struct produto
+{
+    categoria *categoria;  
+    char nome[100];
+    char sku[20];
+    float preco;
+    int quantidade;
+    int produto_numero;
+
+} produto;
+
+typedef struct lista
+{
+    produto *produto;
+    struct lista *proximo;
+    struct lista *anterior;
+} lista;
